0a68e6a2286f2ccd942d8f4180362b19ca7dd3d8
Christophe Lengelé <xon@mbpdechristophe.home>
2022-01-15 09:15:22 -0500

    Highlighting SuperCollidere

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php
+	// Include file to webpage
+	include('sc_highlighting.php');
+	
+	// Add link to formatting stylesheet (argument adds prepath)
+	sc_add_stylesheet();
+	
+	// Run function to add highlighting
+	sc_highlighter("4Live4Life_Extensions/_Init Midi Ak.scd");
+?>
